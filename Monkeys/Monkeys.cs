ab22922ff0643a216d1e8a644ed1af9735db643f
lilbig9 <lilbig9@gmail.com>
2017-11-24 17:41:25 +0300

    first commit

---
+++ Monkeys/Monkeys.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monkeys
+{
+    public abstract class Monkey
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Sizeoftale { get; set; }
+        public int Weight { get; set; }
+    }
+
+    public class BigMonkey : Monkey
+    {
+        private int tale = 50;
+    }
+
+    public class LittleMonkey : Monkey
+    {
+        private int tale = 20;
+    }
+
+    public class Gibbon : BigMonkey
+    {
+        public Gibbon()
+        {
+            Name = "Гиббон";
+            Age = 10;
+            Weight = 50;
+            Sizeoftale = 20;
+        }
+    }
+
+    public class Baboon : BigMonkey
+    {
+        public Baboon()
+        {
+            Name = "Бабуин";
+            Age = 20;
+            Weight = 30;
+            Sizeoftale = 13;
+        }
+    }
+    public class Orangitan : LittleMonkey
+    {
+        public Orangitan()
+        {
+            Name = "Орангутан";
+            Age = 30;
+            Weight = 70;
+            Sizeoftale = 5;
+        }
+    }
+    public class Chimpanzee : LittleMonkey
+    {
+        public Chimpanzee()
+        {
+            Name = "Шимпанзе";
+            Age = 40;
+            Weight = 20;
+            Sizeoftale = 22;
+        }
+    }
+}
---
+++ Monkeys/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monkeys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var monkey = new List<Monkey>
+            {
+                new Gibbon(),
+                new Baboon(),
+                new Orangitan(),
+                new Chimpanzee()
+            };
+
+            foreach (var Monkey in monkey)
+            {
+                if (Monkey.Sizeoftale <= 10)
+                {
+                    Console.WriteLine($"У {Monkey.Name} маленький хвост, всего {Monkey.Sizeoftale} см.");
+                }
+                else if (Monkey.Sizeoftale >= 11)
+                {
+                    Console.WriteLine($"У {Monkey.Name} большой хвост, целых {Monkey.Sizeoftale} см.");
+                }
+
+                if (Monkey.Age <= 20)
+                {
+                    Console.WriteLine($"{Monkey.Name} молодая особь, всего {Monkey.Age} лет.");
+                }
+                else if (Monkey.Age >= 21)
+                {
+                    Console.WriteLine($"{Monkey.Name} старая особь, целых {Monkey.Age} лет.");
+                }
+
+                if (Monkey.Weight <= 40)
+                {
+                    Console.WriteLine($"{Monkey.Name} худой, всего {Monkey.Weight} кг.");
+                }
+                else if (Monkey.Weight > 41)
+                {
+                    Console.WriteLine($"{Monkey.Name} толстый, целых {Monkey.Weight} кг.");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
